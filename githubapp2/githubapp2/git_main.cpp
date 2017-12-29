88ce251cea29318a0a807e4e412d52c3588d9159
sohrabag <33716625+sohrabag@users.noreply.github.com>
2017-12-29 15:56:41 +0100

    added a for loop and doubly linked list to the code. a loop fills the list with integers.

--- githubapp2/githubapp2/git_main.cpp
+++ githubapp2/githubapp2/git_main.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 
 #pragma warning(disable: 4996)
 
@@ -13,6 +14,10 @@ int main(void)
 	std::time_t t = std::time(nullptr);
 	std::cout << "UTC:       " << std::put_time(std::gmtime(&t), "%c %Z") << '\n';
 	std::cout << "local:     " << std::put_time(std::localtime(&t), "%c %Z") << '\n';
+
+	std::list<int> path;
+	for(int i = 0; i < 10; i++)
+		path.push_back(i);
 
 	return 0;
 }
